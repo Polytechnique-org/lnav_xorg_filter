5259e3a8797f255c82e700bb93479febb2e623e0
Guillaume Girol <symphorien+git@xlumurb.eu>
2021-08-06 12:00:00 +0000

    add script to collect structured info

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 path="$(LANG=C lnav -i mail_log.json 2>&1 | cut -d' ' -f3)"
 if [ -f "$path" ];
 then
-  cp resolve.lnav msgid.lnav "$(dirname "$path")"
+  cp -- *.lnav "$(dirname "$path")"
 else
   echo "don't know where to install scripts"
 fi
